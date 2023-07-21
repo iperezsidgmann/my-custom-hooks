9836fedc21723b049ad2881def762cd9c4778534
iperezsidgmann <iperezsidgmann@gmail.com>
2023-07-21 13:27:44 -0400

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatchTodos] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        };
+
+        dispatchTodos(action);
+    };
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodos({
+            type: '[TODO] Remove Todo',
+            payload: id,
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        dispatchTodos({
+            type: '[TODO] Toggle Todo',
+            payload: id,
+        });
+    }
+
+    const todosCount = () => {
+        todos.length
+    }
+
+    const pendingTodosCount = () => {
+        todos.filter(todo => !todo.done).length
+    }
+
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
